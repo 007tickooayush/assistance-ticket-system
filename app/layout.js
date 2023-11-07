4691088629b8f1292e2d523f30de20070bf6ed93
Ayush Tickoo <ayush.tickoo@ONPASSIVE.ORG>
2023-11-07 15:05:52 +0530

    designing changes

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,9 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import Nav from "./(components)/Nav";
 
-
+// adding imports to avoid jumpiness(sudden enlargement) of icons
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,9 +17,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Nav/>
-        {children}
-        </body>
+        <div className="flex flex-col h-screen max-h-screen">
+        <Nav />
+        <div className="flex-grow overflow-y-auto bg-page text-default-text">
+            {children}
+        </div>
+        </div>
+      </body>
     </html>
   );
 }
