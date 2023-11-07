95d4496b1988a7aa9daef0be817c8fb56d5317a5
Ayush Tickoo <ayush.tickoo@ONPASSIVE.ORG>
2023-11-07 15:26:46 +0530

    progress bar+priority of ticket in ticket card

---
+++ app/(components)/PreiorityDisplay.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import { faFire } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+const PreiorityDisplay = () => {
+    return (
+        <div className='flex justify-start align-baseline'>
+            <FontAwesomeIcon icon={faFire} className='text-red-400'/>
+            <FontAwesomeIcon icon={faFire} className='text-red-400'/>
+            <FontAwesomeIcon icon={faFire} className='text-red-400'/>
+            <FontAwesomeIcon icon={faFire} className='text-red-400'/>
+            <FontAwesomeIcon icon={faFire} className='text-red-400'/>
+        </div>
+    )
+}
+
+export default PreiorityDisplay;
--- app/(components)/TicketCard.jsx
+++ app/(components)/TicketCard.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import DeleteBlock from './DeleteBlock';
+import PreiorityDisplay from './PreiorityDisplay';
+import ProgressDisplay from './ProgressDisplay';
 
 const TicketCard = () => {
   return (
     <div>
         <DeleteBlock />
+        <PreiorityDisplay />
+        <ProgressDisplay />
     </div>
   )
 }
