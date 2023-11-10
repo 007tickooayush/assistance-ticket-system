70c35c6199a809ff26eb2e797358fc0e7d54758a
Ayush Tickoo <ayush.tickoo@ONPASSIVE.ORG>
2023-11-10 12:31:59 +0530

    lexical design for status display in TicketCard

--- app/(components)/StatusDisplay.jsx
+++ app/(components)/StatusDisplay.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
 const StatusDisplay = ({ status }) => {
+    const statusDisp = status.split().map(w => w.replace(w[0],w[0].toUpperCase())).toString().concat()
+
     return (
         <span className='inline-block rounded-full px-2 py-1 text-xs font-semibold text-gray-700 bg-green-200'>
-            {status}
+            {statusDisp}
         </span>
     )
 }
